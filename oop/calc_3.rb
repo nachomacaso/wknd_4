0761ffe44587232493e44c3dbc18bc072b448324
Nate Macaso <nathanmacaso@gmail.com>
2016-09-17 21:29:31 -0500

    Initial commit

--- oop/calc_3.rb
+++ oop/calc_3.rb
@@ -4,83 +4,107 @@
 # DRY up all the code below - there shouldn't be a single method duplicated between
 # any two classes.
 
-class SimpleCalculator
-
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
-
-end
-
-class FancyCalculator
-
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
-
+module FancyCalculator
   def square_root(number)
     Math.sqrt(number)
   end
-
 end
 
-class WhizBangCalculator
-
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
-
-  def square_root(number)
-    Math.sqrt(number)
-  end
-
+module WhizBangCalculator
   def hypotenuse(first_number, second_number)
     Math.hypot(first_number, second_number)
   end
   
   def exponent(first_number, exponent_number)
     total = 1
-    exponent_number.times { total = multiply(total,first_number) }
+    exponent_number.times { total = multiply(total, first_number) }
     total
   end
+end
 
+class SimpleCalculator
+  include FancyCalculator
+  include WhizBangCalculator
+
+  def add(first_number, second_number)
+    first_number + second_number
+  end
+
+  def subtract(first_number, second_number)
+    first_number - second_number
+  end
+
+  def multiply(first_number, second_number)
+    first_number * second_number
+  end
+
+  def divide(first_number, second_number)
+    first_number / second_number
+  end
 end
 
 # Copy your driver code from the previous exercise and more below:
+puts "TESTING calc_3..."
+puts
 
+calculator = SimpleCalculator.new
+
+result = calculator.add(5, 2)
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 7
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = calculator.multiply(5, 2)
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 10
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = calculator.square_root(81)
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 9.0
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = calculator.hypotenuse(3, 4)
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 5.0
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = calculator.exponent(3, 4)
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 81
+  puts "PASS!"
+else
+  puts "F"
+end
