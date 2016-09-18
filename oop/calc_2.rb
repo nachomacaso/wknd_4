cb5e1f82f59b78126f7d7919ce504ac7ade8f358
Nate Macaso <nathanmacaso@gmail.com>
2016-09-17 19:53:38 -0500

    Initial commit

--- oop/calc_2.rb
+++ oop/calc_2.rb
@@ -3,8 +3,7 @@
 # a different technique using Composition. Hint: Google 'Ruby Mixin Module'. (Using
 # mixin modules is how you achieve Composition with Ruby.)
 
-class SimpleCalculator
-
+module SimpleCalculator
   def add(first_number, second_number)
     first_number + second_number
   end
@@ -20,32 +19,53 @@ class SimpleCalculator
   def divide(first_number, second_number)
     first_number / second_number
   end
-
 end
 
 class FancyCalculator
-
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
-
+  include SimpleCalculator
   def square_root(number)
     Math.sqrt(number)
   end
-
 end
 
 # Copy your driver code from the previous exercise below:
+puts "TESTING calc_2..."
+puts
 
+calculator = FancyCalculator.new
+
+result = calculator.add(5, 2)
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 7
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = calculator.multiply(5, 2)
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 10
+  puts "PASS!"
+else
+  puts "F"
+end
+
+result = calculator.square_root(25)
+
+puts "Your method returned:"
+puts result
+puts
+
+if result == 5.0
+  puts "PASS!"
+else
+  puts "F"
+end
